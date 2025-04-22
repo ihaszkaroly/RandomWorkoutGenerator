009bfe430189f4c4b9408ce60b9fe0b7c3c37e77
ihaszkaroly <90614087+ihaszkaroly@users.noreply.github.com>
2025-04-22 18:52:54 +0200

    improved workout generation

--- src/Main.fs
+++ src/Main.fs
@@ -1,4 +1,4 @@
-module Main
+﻿module Main
 
 open Feliz
 open Elmish
@@ -6,35 +6,43 @@ open Elmish.React
 
 type Workout = {
     Name: string
-    Reps: string
+    RepsNormal: string
+    RepsIntense: string
 }
 
 type Model = {
-    CurrentWorkout: Workout option
+    WorkoutPlan: Workout list
+    IsIntense: bool
 }
 
 type Msg =
-    | GenerateWorkout
+    | GenerateWorkoutPlan
+    | ToggleIntensity of bool
 
 let allWorkouts = [
-    { Name = "Push-ups"; Reps = "3 x 15" }
-    { Name = "Squats"; Reps = "4 x 20" }
-    { Name = "Burpees"; Reps = "3 x 10" }
-    { Name = "Plank"; Reps = "2 x 1 min" }
-    { Name = "Jumping Jacks"; Reps = "3 x 30" }
-    { Name = "Lunges"; Reps = "3 x 12 per leg" }
-    { Name = "Mountain Climbers"; Reps = "3 x 40" }
-    { Name = "High Knees"; Reps = "3 x 30 sec" }
+    { Name = "Push-ups"; RepsNormal = "3 x 15"; RepsIntense = "5 x 20" }
+    { Name = "Squats"; RepsNormal = "4 x 20"; RepsIntense = "6 x 25" }
+    { Name = "Burpees"; RepsNormal = "3 x 10"; RepsIntense = "5 x 15" }
+    { Name = "Plank"; RepsNormal = "2 x 1 min"; RepsIntense = "3 x 90 sec" }
+    { Name = "Jumping Jacks"; RepsNormal = "3 x 30"; RepsIntense = "5 x 50" }
+    { Name = "Lunges"; RepsNormal = "3 x 12 per leg"; RepsIntense = "5 x 15 per leg" }
+    { Name = "Mountain Climbers"; RepsNormal = "3 x 40"; RepsIntense = "5 x 60" }
+    { Name = "High Knees"; RepsNormal = "3 x 30 sec"; RepsIntense = "5 x 45 sec" }
 ]
 
-let init () = { CurrentWorkout = None }, Cmd.none
+let init () = { WorkoutPlan = []; IsIntense = false }, Cmd.none
 
 let update (msg: Msg) (model: Model) =
     match msg with
-    | GenerateWorkout ->
+    | GenerateWorkoutPlan ->
         let rnd = System.Random()
-        let workout = allWorkouts.[rnd.Next(allWorkouts.Length)]
-        { model with CurrentWorkout = Some workout }, Cmd.none
+        let plan =
+            allWorkouts
+            |> List.sortBy (fun _ -> rnd.Next()) // Shuffle
+            |> List.take 3
+        { model with WorkoutPlan = plan }, Cmd.none
+    | ToggleIntensity state ->
+        { model with IsIntense = state }, Cmd.none
 
 let view (model: Model) (dispatch: Msg -> unit) =
     Html.div [
@@ -45,33 +53,53 @@ let view (model: Model) (dispatch: Msg -> unit) =
             style.justifyContent.center
             style.height (length.vh 100)
             style.fontFamily "Segoe UI"
+            style.padding 20
         ]
         prop.children [
 
             Html.h1 [
-                prop.text "Random Workout Generator"
+                prop.text "Random Workout Plan Generator"
+                prop.style [ style.marginBottom 10 ]
+            ]
+
+            Html.div [
                 prop.style [ style.marginBottom 20 ]
+                prop.children [
+                    Html.label [
+                        Html.input [
+                            prop.type'.checkbox
+                            prop.isChecked model.IsIntense
+                            prop.onChange (fun ev -> dispatch (ToggleIntensity ev))
+                        ]
+                        Html.span [
+                            prop.text " Intense Mode"
+                            prop.style [ style.marginLeft 10 ]
+                        ]
+                    ]
+                ]
             ]
 
             Html.button [
-                prop.text "Generate Workout"
+                prop.text "Generate Workout Plan"
                 prop.style [
                     style.padding (10, 20)
                     style.fontSize 18
                     style.cursor.pointer
                     style.marginBottom 30
                 ]
-                prop.onClick (fun _ -> dispatch GenerateWorkout)
+                prop.onClick (fun _ -> dispatch GenerateWorkoutPlan)
             ]
 
-            match model.CurrentWorkout with
-            | Some workout ->
+            if model.WorkoutPlan.IsEmpty then
+                Html.p "Click the button to generate your workout plan!"
+            else
                 Html.div [
-                    Html.h2 workout.Name
-                    Html.p workout.Reps
+                    for workout in model.WorkoutPlan ->
+                        Html.div [
+                            Html.h2 workout.Name
+                            Html.p (if model.IsIntense then workout.RepsIntense else workout.RepsNormal)
+                        ]
                 ]
-            | None ->
-                Html.p "Click the button to get your workout!"
         ]
     ]
 
